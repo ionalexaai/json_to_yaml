b02c122218a1fa84021235ec48626a703a31d8e4
Alexandru Ion <ionalexaai@gmail.com>
2025-07-06 00:24:10 +0300

    Refactor routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from routes import register_resources
+from app.routes import register_resources
 
 def create_app():
     """Application factory for creating Flask app instances"""
